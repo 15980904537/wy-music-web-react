b2b1956b211d8f4950a7dfcb2e29fa440e9d7531
15980904537 <2565051131@qq.com>
2021-08-15 17:17:26 +0800

    封装axios

---
+++ src/servers/axios.js
@@ -0,0 +1,41 @@
+import axios from 'axios'
+
+export const require = function require(option) { 
+  return new Promise((resolve,reject) => { 
+    const instance = axios.create({
+      baseURL: 'http://123.207.32.32:9001/',
+      timeout: 10000
+    })
+
+    instance.interceptors.request.use((config) => { 
+      return config
+    }, err => { 
+      return err
+    })
+    instance.interceptors.response.use(response => { 
+      return response.data
+    }, err => { 
+        if (err && err.response) { 
+          switch (err.response.status) {
+            case 400:
+              err.message = '请求错误'
+              break
+            case 401:
+              err.message = '未授权的访问'
+              break
+            default:
+              err.message = "其他错误信息"
+          }
+        }
+        return err
+    })
+
+    instance(option).then((result) => { 
+        resolve(result)
+    }, err => { 
+        reject(err)
+    })
+  })
+}
+
+ 
